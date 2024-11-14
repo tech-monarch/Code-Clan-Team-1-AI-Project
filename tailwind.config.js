43daffce5307a83305ea5f9db2cc3743e6590823
amnesia2k <tilewaolatoye17@gmail.com>
2024-11-14 11:41:20 +0100

    set up default colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customBlack: "#000000",
+        customPink: "#F21F7B",
+      },
+    },
   },
   plugins: [],
 };
